7da04348c0f208dba454c8543c5c8ede49074758
KoalaCoder11 <159075384+KoalaCoder11@users.noreply.github.com>
2024-02-07 09:40:16 -0800

    created game

--- main.ts
+++ main.ts
@@ -1,2 +1,55 @@
+namespace SpriteKind {
+    export const Power = SpriteKind.create()
+    export const Money = SpriteKind.create()
+    export const Life = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Money, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeScoreBy(30)
+})
+info.onScore(60, function () {
+    game.gameOver(true)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.x += 5
+    sprite.y += 5
+    otherSprite.x += -5
+    otherSprite.y += -5
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Life, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(10)
+})
+info.onLifeZero(function () {
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Power, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    sprite.setFlag(SpriteFlag.Ghost, true)
+    pause(5000)
+    sprite.setFlag(SpriteFlag.Ghost, false)
+})
 info.setLife(3)
 info.setScore(0)
+scene.setBackgroundImage(assets.image`pond`)
+let nemo = sprites.create(assets.image`nemo`, SpriteKind.Player)
+controller.moveSprite(nemo, 100, 100)
+nemo.setStayInScreen(true)
+let sharks = [sprites.create(assets.image`shark1`, SpriteKind.Enemy), sprites.create(assets.image`shark2`, SpriteKind.Enemy), sprites.create(assets.image`shark3`, SpriteKind.Enemy)]
+sharks[0].setPosition(26, 22)
+sharks[1].setPosition(122, 41)
+sharks[2].setPosition(36, 103)
+let powerups = [sprites.create(assets.image`coin`, SpriteKind.Money), sprites.create(assets.image`heart`, SpriteKind.Life), sprites.create(assets.image`star`, SpriteKind.Power)]
+powerups[0].setPosition(83, 11)
+powerups[1].setPosition(28, 69)
+powerups[2].setPosition(123, 95)
+pause(1000)
+for (let shark of sharks) {
+    shark.follow(nemo, 50)
+}
+game.onUpdateInterval(1000, function () {
+    info.changeScoreBy(1)
+})
